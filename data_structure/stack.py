b43b1868708136acb9feb92c9836de6087deb3aa
김동규 <kimdongkyu@gimdong-gyuui-MacBookAir.local>
2023-08-21 20:06:48 +0900

    ch3구간합~

--- data_structure/stack.py
+++ data_structure/stack.py
@@ -1,14 +1,27 @@
-import sys
-input = sys.stdin.readline
-suNo , quizNo = map(int, input().split())
-numbers = list(map(int, input().split()))
-prefix_sum = [0]
+# 입력받은 정수들 중 i~j번째의 합 구하기
+
+#1. 입력받은 정수를 리스트로 정렬
+user_input = input("원하는 정수를 입력하세요: ")
+
+input_list = user_input.split()
+
+int_list = [int(i) for i in input_list]
+
+print("입력받은 정수를 리스트로 반환합니다. : ", int_list)
+
+#2. i~j번쨰까지의 합은  S(j) - S(i-1)
+Sum_list = [0]
 temp = 0
-
-for i in numbers:
+for i in int_list:
     temp += i
-    prefix_sum.append(temp)
+    Sum_list.append(temp)
 
-for i in range(quizNo):
-    s, e = map(int, input().split())
-    
+user_output = input("어떤 구간의 합을 구할까요?: ")
+output_list = user_output.split()
+int_list2 = [int(i) for i in output_list]
+i = int_list2[0]
+j = int_list2[1]
+def S(i,j):
+    Sum_list[j] - Sum_list[i-1]
+print(S(4,6))
+
