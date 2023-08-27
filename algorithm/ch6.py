e424905e2023c9e193ca5ea35266cbdecae2e9cf
김동규 <kimdongkyu@gimdong-gyuui-MacBookAir.local>
2023-08-27 15:09:49 +0900

    p144

--- algorithm/ch6.py
+++ algorithm/ch6.py
@@ -28,3 +28,29 @@ nclass1.mean()
 #외부변수 이용해 추출(@붙이기)
 var = 3
 df.query('nclass == @var')
+
+import pandas as pd
+import numpy as np
+import sklearn
+
+#배기량이 4이하인 차와 5이상인 차의 hwy 평균 비교
+df = pd.read_csv('mpg.csv')
+df.head()
+low = df.query('displ <= 4')
+high = df.query('displ >= 5')
+print(low['hwy'].mean())
+print(high['hwy'].mean())
+
+df.head()
+#audi, toyota의 도시 연비 평균 비교
+audi = df.query('manufacturer == "audi"')
+toyota = df.query('manufacturer == "toyota"')
+
+if audi['cty'].mean() > toyota['cty'].mean():
+    print('audi')
+else:
+    print('toyota')
+
+#chevrolet, ford, honda 세 브랜드의 hwy 평균
+CFH = df.query('manufacturer in ["chevrolet","ford","honda"]')
+CFH['hwy'].mean()
